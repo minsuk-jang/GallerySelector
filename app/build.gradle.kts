75a08877575004d8b42a27c52b4374696fb4150a
Minsuk <minsuk.jang@influential.co.kr>
2024-02-24 19:43:20 +0900

    change lifecycle, activity-compose gradle version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,8 +53,8 @@ dependencies {
     //implementation(project(":GallerySelector"))
 
     implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
-    implementation("androidx.activity:activity-compose:1.8.2")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
+    implementation("androidx.activity:activity-compose:1.7.2")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
