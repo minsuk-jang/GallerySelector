64faa60ea54cb1efd404be8461c73b12da439b29
Minsuk <minsuk.jang@influential.co.kr>
2024-03-02 10:50:33 +0900

    Remove import file

--- app/src/main/java/com/jms/galleryselector/MainActivity.kt
+++ app/src/main/java/com/jms/galleryselector/MainActivity.kt
@@ -6,19 +6,12 @@ import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color.Companion.Gray
-import androidx.compose.ui.unit.dp
 import androidx.core.content.ContextCompat
-import com.jms.galleryselector.ui.GalleryScreen
 import com.jms.galleryselector.ui.theme.GallerySelectorTheme
-import com.jms.galleryselector.ui.theme.Purple40
 
 class MainActivity : ComponentActivity() {
 
@@ -43,14 +36,14 @@ class MainActivity : ComponentActivity() {
                             arrays[0]
                         ) == PackageManager.PERMISSION_GRANTED
                     ) {
-                        GalleryScreen {
+                       /* GalleryScreen {
                             Box(
                                 modifier = Modifier
                                     .border(width = 3.5.dp, color = Purple40)
                                     .background(color = Gray.copy(0.5f))
                                     .fillMaxSize()
                             )
-                        }
+                        }*/
                     }
                 }
             }
