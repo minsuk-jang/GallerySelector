a801df75474ffbb5d0594dae26377890824ae4dc
Minsuk <minsuk.jang@influential.co.kr>
2024-04-04 21:45:31 +0900

    Library Version up

--- GallerySelector/build.gradle.kts
+++ GallerySelector/build.gradle.kts
@@ -82,7 +82,7 @@ publishing{
         register<MavenPublication>("release"){
             groupId = "com.github.minsuk-jang"
             artifactId = "GallerySelector"
-            version = "1.0.3"
+            version = "1.0.4"
 
             afterEvaluate{
                 from(components["release"])
